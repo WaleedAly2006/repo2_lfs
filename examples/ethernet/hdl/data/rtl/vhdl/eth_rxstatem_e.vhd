d4dc746c98d0cda71e31098e71f49f06f3b0769f
Aly <waly@mgc.mentorg.com>
2022-08-30 17:25:53 +0200

    third commit m3

--- examples/ethernet/hdl/data/rtl/vhdl/eth_rxstatem_e.vhd
+++ examples/ethernet/hdl/data/rtl/vhdl/eth_rxstatem_e.vhd
@@ -1,23 +1,3 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-USE ieee.std_logic_arith.all;
-
-ENTITY eth_rxstatem IS
-    PORT (
-        MRxClk           : IN     std_logic;
-        Reset            : IN     std_logic;
-        MRxDV            : IN     std_logic;
-        ByteCntEq0       : IN     std_logic;
-        ByteCntGreat2    : IN     std_logic;
-        MRxDEq5          : IN     std_logic;
-        Transmitting     : IN     std_logic;
-        MRxDEqD          : IN     std_logic;
-        IFGCounterEq24   : IN     std_logic;
-        ByteCntMaxFrame  : IN     std_logic;
-        StateData        : OUT    std_logic_vector(1 downto 0);
-        StateIdle        : OUT    std_logic;
-        StateDrop        : OUT    std_logic;
-        StatePreamble    : OUT    std_logic;
-        StateSFD         : OUT    std_logic
-    );                  
-END eth_rxstatem;
+version https://git-lfs.github.com/spec/v1
+oid sha256:0eb18f4ee2e7544fb696c219fb0880953cecb0c12ec768a5de7ce2c1f71752a4
+size 867
--- examples/tutorial_ref/Import/Sequencer/Seq_TestBench.v
+++ examples/tutorial_ref/Import/Sequencer/Seq_TestBench.v
@@ -1,24 +1,3 @@
-// Verilog Test Bench for Fibonacci Sequencer 
-
-module fibgen_tb;
-
-// Internal signal declarations
-wire        clock;
-wire [7:0]  monitor;
-wire        reset;
-
-// Instances 
-fibgen UUT( 
-   .clock  (clock), 
-   .fibout (monitor), 
-   .reset  (reset)
-); 
-
-fibgen_tester Checker( 
-   .monitor (monitor), 
-   .clock  (clock), 
-   .reset  (reset)
-); 
-
-endmodule
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:61f10d43534e4d6eddad2a321ae7ca7dc08f9237b7a94fb186bdaaee9b8d0750
+size 382
